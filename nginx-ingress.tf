b59f35916f9112d6d60b5909461718e39377f390
Taiwolawal <taiwolawal360@gmail.com>
2024-06-19 09:22:36 +0100

    update

--- nginx-ingress.tf
+++ nginx-ingress.tf
@@ -1,10 +1,10 @@
-# resource "helm_release" "external_nginx" {
-#   name = "external"
+resource "helm_release" "external_nginx" {
+  name = "external"
 
-#   repository       = "https://kubernetes.github.io/ingress-nginx"
-#   chart            = "ingress-nginx"
-#   namespace        = "ingress"
-#   create_namespace = true
-#   version          = "4.10.1"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.10.1"
 
-# }
+}
